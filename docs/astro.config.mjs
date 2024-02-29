da63a49344d86606183b360df9ba2eca0e89ec00
citrus327 <citrus327@outlook.com>
2024-03-01 00:56:48 +0800

    feat: starlight docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      // TODO: can only use external css file for styling.
+      customCss: ["@blizzbolts/hoardui/dist/index.css"],
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Example Guide",
+              link: "/guides/example/",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+    react(),
+  ],
+});
